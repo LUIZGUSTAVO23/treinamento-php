d5a6389d482502b9ff8db4000b9f9b4f8cd00441
Gustavo <luiz-gustavo-19@hotmail.com>
2018-09-25 15:48:54 -0300

    meus primeiros arquivos enviados

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Teste</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h1 class=" text-center"> Olá mundo!</h1>
+			</div>
+
+			<div class="col-md-3">
+				<a href="https://www.google.com.br/">google</a>
+			</div>
+			
+		</div>
+	</div>
+
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
